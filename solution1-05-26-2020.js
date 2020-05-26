13dc94616634347f529cebd75386ab35bf475d10
Brian E. Peterson <bepetersn@gmail.com>
2020-05-26 09:20:57 -0600

    Initial commit

---
+++ solution1-05-26-2020.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let candidate = n 
+        let direction = 1
+        let wasBad, isBad, step
+        while(true) {
+            // change direction if changed valence
+            wasBad = isBad; isBad = isBadVersion(candidate)
+            direction = (isBad ^ wasBad) ? -1 : 1
+            step = Math.floor(candidate / 2)
+            candidate += (step * direction)
+        }
+        return candidate
+    };
+};
